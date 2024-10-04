3d69ae23268d1e5064c853e8e03d2d22c3fc71c1
Satish9047 <satishprajapati930@gmail.com>
2024-10-04 16:31:30 +0545

    14: Update Proper Naming to authServices

--- pizza-delivery-app/server/src/controllers/auth.controller.ts
+++ pizza-delivery-app/server/src/controllers/auth.controller.ts
@@ -2,18 +2,18 @@ import { Request, Response } from "express";
 
 import { ApiResponse } from "../utils/apiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
-import * as authService from "../services/auth.service";
+import * as authServices from "../services/auth.service";
 
 export const signUpHandler = asyncHandler(
   async (req: Request, res: Response) => {
-    const data = await authService.signUpService(req.body);
+    const data = await authServices.signUpService(req.body);
     res.json(new ApiResponse(201, "User created successfully", data));
   }
 );
 
 export const signInHandler = asyncHandler(
   async (req: Request, res: Response) => {
-    const data = await authService.signInService(req.body);
+    const data = await authServices.signInService(req.body);
     res.json(new ApiResponse(200, "User sign in successfully", data));
   }
 );
