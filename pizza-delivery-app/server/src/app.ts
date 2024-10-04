60f5db068941f6846aa293aa7cfc05d5247688cc
Satish9047 <satishprajapati930@gmail.com>
2024-10-04 16:23:53 +0545

    08: Add Router to the Express App

--- pizza-delivery-app/server/src/app.ts
+++ pizza-delivery-app/server/src/app.ts
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 
+import router from "./routes";
 import errorHandler from "./middlewares/error.middleware";
 
 const app = express();
@@ -13,9 +14,10 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
-app.use("/api/v1", (req: Request, res: Response) => {
+app.get("/api/v1", (req: Request, res: Response) => {
   res.send("Hello Pizza Lovers");
 });
+app.use("/api/v1", router);
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   errorHandler(err, req, res, next);
