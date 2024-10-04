f5d01ce05249a9b1f9f070f0035553c6bca1dca8
Satish9047 <satishprajapati930@gmail.com>
2024-10-04 16:29:13 +0545

    11: Update AuthController with AsyncHandler and Use AuthService

--- pizza-delivery-app/server/src/controllers/auth.controller.ts
+++ pizza-delivery-app/server/src/controllers/auth.controller.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from "express";
 
-export const signUpHandler = async (req: Request, res: Response) => {
-  res.send("This is sign up handler");
-};
+import { ApiResponse } from "../utils/apiResponse";
+import { asyncHandler } from "../utils/asyncHandler";
+import * as authService from "../services/auth.service";
 
-export const signInHandler = async (req: Request, res: Response) => {
-  res.send("This is sign in handler");
-};
+export const signUpHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await authService.signUpService(req.body);
+    res.json(new ApiResponse(201, "User created successfully", data));
+  }
+);
+
+export const signInHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await authService.signInService(req.body);
+    res.json(new ApiResponse(200, "User sign in successfully", data));
+  }
+);
