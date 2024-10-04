17bf27f24885edeef7a637d2fcf24ecd61bceb12
Satish9047 <satishprajapati930@gmail.com>
2024-10-04 17:14:02 +0545

    18: Add Cookie-Parser to Express App

--- pizza-delivery-app/server/src/app.ts
+++ pizza-delivery-app/server/src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import router from "./routes";
 import errorHandler from "./middlewares/error.middleware";
@@ -13,6 +14,7 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(cookieParser());
 
 app.get("/api/v1", (req: Request, res: Response) => {
   res.send("Hello Pizza Lovers");
