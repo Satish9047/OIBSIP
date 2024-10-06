e9afc89eca9efb58597bacb8ba6f178b7009071c
Satish9047 <satishprajapati930@gmail.com>
2024-10-06 17:46:59 +0545

    37: Add VerifyRefreshToken Middleware

--- pizza-delivery-app/server/src/middlewares/jwt.middleware.ts
+++ pizza-delivery-app/server/src/middlewares/jwt.middleware.ts
@@ -36,4 +36,31 @@ const verifyToken = async (
   }
 };
 
-export { verifyToken };
+const verifyRefreshToken = async (
+  req: Request & { user?: JwtPayload },
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.refreshToken;
+  if (!token) {
+    throw new ApiError(401, "Unauthorized Request");
+  }
+  try {
+    const decoded = jwt.verify(token, appConfig.jwtSecret) as JwtPayload;
+    if (!decoded || !decoded.id || !decoded.email) {
+      throw new ApiError(401, "Invalid token");
+    }
+    req.user = decoded as JwtUser;
+    next();
+  } catch (error) {
+    if (error instanceof TokenExpiredError) {
+      throw new ApiError(401, "Token expired");
+    } else if (error instanceof JsonWebTokenError) {
+      throw new ApiError(401, "Invalid token");
+    } else {
+      throw new ApiError(500, "Internal server error");
+    }
+  }
+};
+
+export { verifyToken, verifyRefreshToken };
