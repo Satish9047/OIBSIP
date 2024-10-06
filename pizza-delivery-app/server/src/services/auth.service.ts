da525655745dd2c9e5cb5d919e60307c34bec1aa
Satish9047 <satishprajapati930@gmail.com>
2024-10-06 17:18:31 +0545

    33: Add UserVerificationService

--- pizza-delivery-app/server/src/services/auth.service.ts
+++ pizza-delivery-app/server/src/services/auth.service.ts
@@ -3,11 +3,15 @@ import { ApiError } from "../utils/apiResponse";
 import { ISignIn, IUser } from "../interface/app.interface";
 import { createJwtToken } from "../utils/jwtToken";
 
-export const signUpService = async (signUpData: IUser) => {
+export const signUpService = async (
+  signUpData: IUser,
+  verificationCode: number
+) => {
   const userExist = await User.findOne({ email: signUpData.email });
   if (userExist) {
     throw new ApiError(400, "User already exist");
   }
+  signUpData.verificationToken = verificationCode;
   const user = await User.create(signUpData);
   const { password, ...userInfo } = user.toObject();
   const { accessToken, refreshToken } = createJwtToken(
@@ -35,4 +39,16 @@ export const signInService = async (signInData: ISignIn) => {
     userInfo.email
   );
   return { userInfo, accessToken, refreshToken };
+};
+
+export const userVerificationService = async (email: String, token: number) => {
+  const userExist = await User.findOne({ email });
+  if (!userExist) {
+    throw new ApiError(404, "User not found");
+  }
+  if (userExist.verificationToken !== token) {
+    throw new ApiError(400, "Invalid verification code");
+  }
+  const user = await User.findOneAndUpdate({ email }, { isVerified: true });
+  return user;
 };
