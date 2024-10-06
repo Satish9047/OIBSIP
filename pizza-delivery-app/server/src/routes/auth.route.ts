75dd678ac281feec93cd11558e821d392d26c093
Satish9047 <satishprajapati930@gmail.com>
2024-10-06 17:19:09 +0545

    34: Add User-Verify Route

--- pizza-delivery-app/server/src/routes/auth.route.ts
+++ pizza-delivery-app/server/src/routes/auth.route.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 
-import { signUpHandler, signInHandler } from "../controllers/auth.controller";
-import { validateData } from "../middlewares/validation.middleware";
+import {
+  signUpHandler,
+  signInHandler,
+  verificationHandler,
+} from "../controllers/auth.controller";
+import { verifyToken } from "../middlewares/jwt.middleware";
 import { signInSchema, signUpSchema } from "../schema/user.schema";
+import { validateData } from "../middlewares/validation.middleware";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", validateData(signUpSchema), signUpHandler);
 authRouter.post("/sign-in", validateData(signInSchema), signInHandler);
+authRouter.post("/verify-user", verifyToken, verificationHandler);
 
 export { authRouter };
