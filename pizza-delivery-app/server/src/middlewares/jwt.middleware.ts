97167cbe36e4a7b1262d9ddfc936ef52dfe81b0a
Satish9047 <satishprajapati930@gmail.com>
2024-10-06 17:13:32 +0545

    29: Add VerifyToken Middleware Function

---
+++ pizza-delivery-app/server/src/middlewares/jwt.middleware.ts
@@ -0,0 +1,39 @@
+import jwt, {
+  JsonWebTokenError,
+  JwtPayload,
+  TokenExpiredError,
+} from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+
+import { ApiError } from "../utils/apiResponse";
+import { appConfig } from "../configs/app.config";
+import { JwtUser } from "../interface/app.interface";
+
+const verifyToken = async (
+  req: Request & { user?: JwtPayload },
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.accessToken;
+  if (!token) {
+    throw new ApiError(401, "Unauthorized");
+  }
+  try {
+    const decoded = jwt.verify(token, appConfig.jwtSecret) as JwtPayload;
+    if (!decoded || !decoded.id || !decoded.email) {
+      throw new ApiError(401, "Invalid token");
+    }
+    req.user = decoded as JwtUser;
+    next();
+  } catch (error) {
+    if (error instanceof TokenExpiredError) {
+      throw new ApiError(401, "Token expired");
+    } else if (error instanceof JsonWebTokenError) {
+      throw new ApiError(401, "Invalid token");
+    } else {
+      throw new ApiError(500, "Internal server error");
+    }
+  }
+};
+
+export { verifyToken };
