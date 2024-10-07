b6053b94b91b8561138ce7a947c562f7f7e0cd7d
Satish9047 <satishprajapati930@gmail.com>
2024-10-07 13:23:00 +0545

    44: Add Inventory Route

--- pizza-delivery-app/server/src/routes/index.ts
+++ pizza-delivery-app/server/src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import { authRouter } from "./auth.route";
 import { userRouter } from "./user.route";
 import { orderRouter } from "./order.route";
+import { inventoryRoute } from "./inventory";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
 router.use("/order", orderRouter);
+router.use("/inventory", inventoryRoute);
 
 export default router;
