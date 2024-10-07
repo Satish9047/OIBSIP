7cc52027e90ebe4caf04d60cb42989afa9717098
Satish9047 <satishprajapati930@gmail.com>
2024-10-07 13:24:49 +0545

    46: Add Recipe Controller + Service

---
+++ pizza-delivery-app/server/src/controllers/inventory.controller.ts
@@ -0,0 +1,114 @@
+import { Request, Response } from "express";
+
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiResponse } from "../utils/apiResponse";
+import * as recipeService from "../services/inventory.service";
+
+export const addPizzaBaseHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await recipeService.addPizzaBaseService(req.body);
+    res.json(new ApiResponse(200, "Pizza base added successfully", data));
+  }
+);
+
+export const updatePizzaBaseHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await recipeService.updatePizzaBaseService(
+      req.params.id,
+      req.body
+    );
+    res.json(new ApiResponse(200, "Pizza base updated successfully", data));
+  }
+);
+
+export const addSauceHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await recipeService.addSauceService(req.body);
+    res.json(new ApiResponse(200, "Sauce added successfully", data));
+  }
+);
+
+export const updateSauceHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await recipeService.updateSauceService(
+      req.params.id,
+      req.body
+    );
+    res.json(new ApiResponse(200, "Sauce updated successfully", data));
+  }
+);
+
+export const addCheeseHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await recipeService.addCheeseService(req.body);
+    res.json(new ApiResponse(200, "Cheese added successfully", data));
+  }
+);
+
+export const updateCheeseHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await recipeService.updateCheeseService(
+      req.params.id,
+      req.body
+    );
+    res.json(new ApiResponse(200, "Cheese updated successfully", data));
+  }
+);
+
+export const addVeggiesHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await recipeService.addVeggiesService(req.body);
+    res.json(new ApiResponse(200, "Veggies added successfully", data));
+  }
+);
+
+export const updateVeggiesHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await recipeService.updateVeggiesService(
+      req.params.id,
+      req.body
+    );
+    res.json(new ApiResponse(200, "Veggies updated successfully", data));
+  }
+);
+
+export const addNonVegHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await recipeService.addNonVegService(req.body);
+    res.json(new ApiResponse(200, "Non-veg added successfully", data));
+  }
+);
+
+export const updateNonVegHandler = asyncHandler(
+  async (req: Request, res: Response) => {
+    const data = await recipeService.updateNonVegService(
+      req.params.id,
+      req.body
+    );
+    res.json(new ApiResponse(200, "Non-veg updated successfully", data));
+  }
+);
+
+// export const getAllRecipeHandler = asyncHandler(
+//   async (req: Request, res: Response) => {
+//     res.status(200).json({
+//       message: "Recipe fetched successfully",
+//     });
+//   }
+// );
+
+// export const createRecipeHandler = asyncHandler(
+//   async (req: Request, res: Response) => {
+//     res.status(200).json({
+//       message: "Recipe created successfully",
+//     });
+//   }
+// );
+
+// export const updateRecipeHandler = asyncHandler(
+//   async (req: Request, res: Response) => {
+//     res.status(200).json({
+//       message: "Recipe updated successfully",
+//     });
+//   }
+// );
---
+++ pizza-delivery-app/server/src/services/inventory.service.ts
@@ -0,0 +1,123 @@
+import { Sauce } from "../models/sauce.model";
+import { Cheese } from "../models/cheese.model";
+import { NonVeg } from "../models/nonVeg.model";
+import { Veggies } from "../models/veggies.model";
+import { PizzaBase } from "../models/pizzaBase.model";
+
+const addPizzaBaseService = async (data: typeof PizzaBase) => {
+  const pizzaBase = await PizzaBase.create(data);
+  return pizzaBase;
+};
+
+const updatePizzaBaseService = async (id: string, quantity: number) => {
+  const base = await PizzaBase.findById(id);
+  if (!base) {
+    throw new Error("Pizza base not found");
+  }
+  const totalQuantity = base.totalQuantity + quantity;
+  const pizzaBase = await PizzaBase.findByIdAndUpdate(
+    id,
+    { totalQuantity },
+    {
+      new: true,
+    }
+  );
+  return pizzaBase;
+};
+
+const addSauceService = async (data: typeof Sauce) => {
+  const sauce = await Sauce.create(data);
+  return sauce;
+};
+
+const updateSauceService = async (id: string, quantity: number) => {
+  const sauceType = await Sauce.findById(id);
+  if (!sauceType) {
+    throw new Error("Sauce not found");
+  }
+  const totalQuantity = sauceType.totalQuantity + quantity;
+  const sauce1 = await Sauce.findByIdAndUpdate(
+    id,
+    { totalQuantity },
+    {
+      new: true,
+    }
+  );
+  return sauce1;
+};
+
+const addCheeseService = async (data: typeof Cheese) => {
+  const cheese = await Cheese.create(data);
+  return cheese;
+};
+
+const updateCheeseService = async (id: string, quantity: number) => {
+  const cheeseType = await Cheese.findById(id);
+  if (!cheeseType) {
+    throw new Error("Cheese not found");
+  }
+  const totalQuantity = cheeseType.totalQuantity + quantity;
+  const cheese = await Cheese.findByIdAndUpdate(
+    id,
+    { totalQuantity },
+    {
+      new: true,
+    }
+  );
+  return cheese;
+};
+
+const addVeggiesService = async (data: typeof Veggies) => {
+  const veggies = await Veggies.create(data);
+  return veggies;
+};
+
+const updateVeggiesService = async (id: string, quantity: number) => {
+  const veggiesType = await Veggies.findById(id);
+  if (!veggiesType) {
+    throw new Error("Veggies not found");
+  }
+  const totalQuantity = veggiesType.totalQuantity + quantity;
+  const veggies = await Veggies.findByIdAndUpdate(
+    id,
+    { totalQuantity },
+    {
+      new: true,
+    }
+  );
+  return veggies;
+};
+
+const addNonVegService = async (data: typeof NonVeg) => {
+  const nonVeg = await NonVeg.create(data);
+  return nonVeg;
+};
+
+const updateNonVegService = async (id: string, quantity: number) => {
+  const nonVegType = await NonVeg.findById(id);
+  if (!nonVegType) {
+    throw new Error("Non-veg not found");
+  }
+  const totalQuantity = nonVegType.totalQuantity + quantity;
+  const nonVeg = await NonVeg.findByIdAndUpdate(
+    id,
+    { totalQuantity },
+    {
+      new: true,
+    }
+  );
+  return nonVeg;
+};
+
+export {
+  addPizzaBaseService,
+  updatePizzaBaseService,
+  addSauceService,
+  updateSauceService,
+  addCheeseService,
+  updateCheeseService,
+  addVeggiesService,
+  updateVeggiesService,
+  addNonVegService,
+  updateNonVegService,
+};
