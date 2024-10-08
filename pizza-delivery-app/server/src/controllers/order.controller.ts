98fc78c8b0b7987f13073e674f22dedc5119b0d8
Satish9047 <satishprajapati930@gmail.com>
2024-10-08 10:12:33 +0545

    53:  Update order controller

--- pizza-delivery-app/server/src/controllers/order.controller.ts
+++ pizza-delivery-app/server/src/controllers/order.controller.ts
@@ -3,17 +3,25 @@ import { Request, Response } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import * as orderServices from "../services/order.service";
 import { ApiResponse } from "../utils/apiResponse";
+import { JwtPayload } from "jsonwebtoken";
+import { JwtUser } from "../interface/app.interface";
 
 export const getAllOrderHandler = asyncHandler(
   async (req: Request, res: Response) => {
+    // console.log("from order:", req.body);
     const data = await orderServices.getAllOrderService();
     res.json(new ApiResponse(200, "Order fetched successful", data));
   }
 );
 
 export const createOrderHandler = asyncHandler(
-  async (req: Request, res: Response) => {
-    const data = await orderServices.createOrderService(req.body);
+  async (req: Request & { user?: JwtUser }, res: Response) => {
+    // console.log("from order:", req.body);
+    if (!req.user) {
+      throw new ApiResponse(401, "Unauthorized");
+    }
+    const user = req.user;
+    const data = await orderServices.createOrderService(user, req.body);
     res.json(new ApiResponse(201, "Order created successful", data));
   }
 );
