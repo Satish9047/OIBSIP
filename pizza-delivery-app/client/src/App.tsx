11531695cfe3520fa688181449b03580338cdd38
Satish9047 <satishprajapati930@gmail.com>
2024-10-11 20:46:35 +0545

    78: Add Nested Route

--- pizza-delivery-app/client/src/App.tsx
+++ pizza-delivery-app/client/src/App.tsx
@@ -7,13 +7,22 @@ import {
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Profile from "./pages/Profile";
+import Order from "./pages/Order";
+import Pizza from "./pages/Pizza";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/">
-      <Route path="/" element={<Home />} />
-      <Route path="sign-in" element={<SignIn />} />
-      <Route path="sign-up" element={<SignUp />} />
+    <Route>
+      <Route path="/" element={<Navbar />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="profile" element={<Profile />}></Route>
+        <Route path="order" element={<Order />}></Route>
+        <Route path="pizza" element={<Pizza />}></Route>
+      </Route>
+      <Route path="sign-in" element={<SignIn />}></Route>
+      <Route path="sign-up" element={<SignUp />}></Route>s
     </Route>
   )
 );
