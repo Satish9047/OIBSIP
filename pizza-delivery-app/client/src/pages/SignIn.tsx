2c1d0787cb03ad468c958bf960ed008d190c262b
Satish9047 <satishprajapati930@gmail.com>
2024-10-12 11:32:12 +0545

    86: Use React Hook Form with Zod Using HookForm Resolver

--- pizza-delivery-app/client/src/pages/SignIn.tsx
+++ pizza-delivery-app/client/src/pages/SignIn.tsx
@@ -1,15 +1,29 @@
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { Link } from "react-router-dom";
+import { FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signInSchema } from "../schema/signInSchema";
 
 const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ resolver: zodResolver(signInSchema) });
+
+  const onSubmit = (formData: FieldValues) => {
+    console.log(formData);
+    reset();
+  };
   return (
     <main className="flex items-center justify-center min-h-screen">
       <section className="w-full max-w-3xl p-8 rounded-lg shadow-md">
         <div className="mb-6 text-center">
           <h1 className="text-3xl font-bold">Sign In Into Super Pizza</h1>
         </div>
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col gap-8 md:flex-row">
             <div className="md:w-1/2">
               <figure>
@@ -26,9 +40,13 @@ const SignIn = () => {
                   Email
                 </label>
                 <Input
+                  {...register("email")}
                   type="email"
                   className="border-stone-300 focus:outline-none focus:border-orange-500"
                 />
+                {errors?.email && (
+                  <p className="text-red-500">{`${errors.email.message}`}</p>
+                )}
               </div>
               <div className="my-4">
                 <label className="text-sm font-medium text-stone-600">
@@ -36,8 +54,12 @@ const SignIn = () => {
                 </label>
                 <Input
                   type="password"
+                  {...register("password")}
                   className="border-stone-300 focus:outline-none focus:border-orange-500"
                 />
+                {errors?.password && (
+                  <p className="text-red-500">{`${errors.password.message}`}</p>
+                )}
               </div>
               <div className="flex justify-between mt-6">
                 <p>
@@ -49,7 +71,10 @@ const SignIn = () => {
                     Sign Up
                   </Link>
                 </p>
-                <Button className="bg-orange-500 rounded-md hover:bg-orange-600 ">
+                <Button
+                  type="submit"
+                  className="bg-orange-500 rounded-md hover:bg-orange-600 "
+                >
                   Sign In
                 </Button>
               </div>
--- pizza-delivery-app/client/src/pages/SignUp.tsx
+++ pizza-delivery-app/client/src/pages/SignUp.tsx
@@ -1,15 +1,31 @@
 import { Link } from "react-router-dom";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
+import { FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signUpSchema } from "../schema/signUpSchema";
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: zodResolver(signUpSchema),
+  });
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <main className="flex items-center justify-center min-h-screen">
       <section className="w-full max-w-3xl p-8 rounded-lg shadow-md">
         <div className="mb-6 text-center">
           <h1 className="text-3xl font-bold">Sign Up For Super Pizza</h1>
         </div>
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col gap-8 md:flex-row">
             <div className="md:w-1/2">
               <figure>
@@ -27,8 +43,12 @@ const SignUp = () => {
                 </label>
                 <Input
                   type="text"
+                  {...register("name")}
                   className=" border-stone-300 focus:outline-none focus:border-orange-500"
                 />
+                {errors?.name && (
+                  <p className="text-red-500">{`${errors.name.message}`}</p>
+                )}
               </div>
               <div>
                 <div className="my-4">
@@ -37,8 +57,12 @@ const SignUp = () => {
                   </label>
                   <Input
                     type="email"
+                    {...register("email")}
                     className=" border-stone-300 focus:outline-none focus:border-orange-500"
                   />
+                  {errors?.email && (
+                    <p className="text-red-500">{`${errors.email.message}`}</p>
+                  )}
                 </div>
                 <div className="my-4">
                   <label className="text-sm font-medium text-stone-600">
@@ -46,8 +70,12 @@ const SignUp = () => {
                   </label>
                   <Input
                     type="number"
+                    {...register("phone")}
                     className=" border-stone-300 focus:outline-none focus:border-orange-500"
                   />
+                  {errors?.phone && (
+                    <p className="text-red-500">{`${errors.phone.message}`}</p>
+                  )}
                 </div>
                 <div className="my-4">
                   <label className="text-sm font-medium text-stone-600">
@@ -55,8 +83,12 @@ const SignUp = () => {
                   </label>
                   <Input
                     type="text"
+                    {...register("address")}
                     className=" border-stone-300 focus:outline-none focus:border-orange-500"
                   />
+                  {errors?.address && (
+                    <p className="text-red-500">{`${errors.address.message}`}</p>
+                  )}
                 </div>
                 <div className="my-4">
                   <label className="text-sm font-medium text-stone-600">
@@ -64,8 +96,12 @@ const SignUp = () => {
                   </label>
                   <Input
                     type="password"
+                    {...register("password")}
                     className=" border-stone-300 focus:outline-none focus:border-orange-500"
                   />
+                  {errors?.password && (
+                    <p className="text-red-500">{`${errors.password.message}`}</p>
+                  )}
                 </div>
                 <div className="flex justify-between mt-6">
                   <p>
@@ -77,7 +113,10 @@ const SignUp = () => {
                       Sign In
                     </Link>
                   </p>
-                  <Button className="bg-orange-500 rounded-md hover:bg-orange-600">
+                  <Button
+                    type="submit"
+                    className="bg-orange-500 rounded-md hover:bg-orange-600"
+                  >
                     Sign Up
                   </Button>
                 </div>
@@ -89,5 +128,4 @@ const SignUp = () => {
     </main>
   );
 };
-
 export default SignUp;
