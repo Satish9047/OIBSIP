0d415b48c2c53d9fce62ef7744fd077a37b55b11
Satish9047 <satishprajapati930@gmail.com>
2024-10-12 11:32:35 +0545

    87: Update Navbar

--- pizza-delivery-app/client/src/components/Navbar.tsx
+++ pizza-delivery-app/client/src/components/Navbar.tsx
@@ -4,14 +4,18 @@ import { Outlet } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div>
-        <div>
-          <Link to={"/"}>Super Pizza</Link>
-        </div>
-        <div></div>
-        <div>
+      <div className="flex justify-center ">
+        <div className="flex items-center justify-between w-11/12 p-4 ">
+          <div>
+            <Link to={"/"}>Super Pizza</Link>
+          </div>
+          <div>
+            <p>Satish@gmail.com</p>
+          </div>
+          {/* <div className="flex-row hidden gap-4 md:flex">
           <Link to={"/sign-in"}>Sign In</Link>
           <Link to={"/sign-up"}>Sign Up</Link>
+        </div> */}
         </div>
       </div>
       <Outlet />
