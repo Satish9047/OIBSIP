8deafa171f6be107fe7f7e7d28a3f5fdc2a5dea0
Satish9047 <satishprajapati930@gmail.com>
2024-10-12 21:57:50 +0545

    92: Update and Create Proper ApiSlice

--- pizza-delivery-app/client/src/redux/apiServices.ts
+++ pizza-delivery-app/client/src/redux/apiServices.ts
@@ -1,17 +1,36 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { IAuthResponse, ISignIn, ISignUp } from "../interface/app.interface";
 
 //Slice
 export const apiServices = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:000/api",
+    baseUrl: "http://localhost:8080/api/v1",
   }),
   endpoints: (builder) => ({
-    signUp: builder.mutation({
-      query: () => `auth/sign-up`,
+    signUp: builder.mutation<IAuthResponse, ISignUp>({
+      query: (credentials) => ({
+        url: "/auth/sign-up",
+        method: "POST",
+        body: credentials,
+      }),
+      transformResponse: (response: IAuthResponse) => response,
+      transformErrorResponse: (error: {
+        status: number;
+        data: IAuthResponse;
+      }) => error.data,
     }),
-    signIn: builder.mutation({
-      query: () => `auth/sign-in`,
+    signIn: builder.mutation<IAuthResponse, ISignIn>({
+      query: (credentials) => ({
+        url: "/auth/sign-in",
+        method: "POST",
+        body: credentials,
+      }),
+      transformResponse: (response: IAuthResponse) => response,
+      transformErrorResponse: (error: {
+        status: number;
+        data: IAuthResponse;
+      }) => error.data,
     }),
   }),
 });
