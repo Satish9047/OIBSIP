33826044b7356e6ebe3089653ff015f822ada74c
Satish9047 <satishprajapati930@gmail.com>
2024-10-14 22:02:44 +0545

    109: Use Selector to use the userState in Navbar Component

--- pizza-delivery-app/client/src/components/Navbar.tsx
+++ pizza-delivery-app/client/src/components/Navbar.tsx
@@ -1,21 +1,29 @@
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 const Navbar = () => {
+  const user = useSelector((state: RootState) => state.user);
   return (
     <>
       <div className="flex justify-center ">
         <div className="flex items-center justify-between w-11/12 p-4 ">
           <div>
-            <Link to={"/"}>Super Pizza</Link>
+            <Link to={"/"}>
+              <label className="text-2xl">Super Pizza</label>
+            </Link>
           </div>
-          <div>
-            <p>Satish@gmail.com</p>
-          </div>
-          {/* <div className="flex-row hidden gap-4 md:flex">
-          <Link to={"/sign-in"}>Sign In</Link>
-          <Link to={"/sign-up"}>Sign Up</Link>
-        </div> */}
+          {user ? (
+            <div>
+              <p>{user.email}</p>
+            </div>
+          ) : (
+            <div className="flex-row hidden gap-4 md:flex">
+              <Link to={"/sign-in"}>Sign In</Link>
+              <Link to={"/sign-up"}>Sign Up</Link>
+            </div>
+          )}
         </div>
       </div>
       <Outlet />
