2039567a5a34c36bc5ffb8a8d3494b9ab02070c3
Satish9047 <satishprajapati930@gmail.com>
2024-10-15 21:52:16 +0545

    112: Update Navbar with auth/me apiService

--- pizza-delivery-app/client/src/components/Navbar.tsx
+++ pizza-delivery-app/client/src/components/Navbar.tsx
@@ -1,13 +1,31 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { RootState } from "../store/store";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { addUserState } from "../redux/state/userSlice";
+import { AppDispatch, RootState } from "../store/store";
+import { useGetMeQuery } from "../redux/apiServices";
 
 const Navbar = () => {
+  const dispatch = useDispatch<AppDispatch>();
   const user = useSelector((state: RootState) => state.user);
+  const { data: userData } = useGetMeQuery({});
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userData && userData.success) {
+      console.log("from navbar useEffect", userData);
+      dispatch(addUserState(userData.data));
+    } else {
+      navigate("/sign-in");
+    }
+  }, [userData, dispatch, navigate]);
   return (
     <>
-      <div className="flex justify-center ">
+      <div className="flex justify-center md:h-20">
         <div className="flex items-center justify-between w-11/12 p-4 ">
           <div>
             <Link to={"/"}>
@@ -30,5 +48,4 @@ const Navbar = () => {
     </>
   );
 };
-
 export default Navbar;
