e11bb3f4a13b56cc5971fbdfb8bd88dccbdb8efa
Satish9047 <satishprajapati930@gmail.com>
2024-10-15 21:51:23 +0545

    111: Update the cors with origin and credentials

--- pizza-delivery-app/server/src/app.ts
+++ pizza-delivery-app/server/src/app.ts
@@ -11,7 +11,12 @@ const app = express();
 
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
