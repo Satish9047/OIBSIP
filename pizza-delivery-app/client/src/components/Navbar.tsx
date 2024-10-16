8305c5c7de887ef19964b111f3cb707048aa58f5
Satish9047 <satishprajapati930@gmail.com>
2024-10-16 20:38:42 +0545

    118: Update Navbar with Best practices

--- pizza-delivery-app/client/src/components/Navbar.tsx
+++ pizza-delivery-app/client/src/components/Navbar.tsx
@@ -2,27 +2,17 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
 
-import { addUserState } from "../redux/state/userSlice";
-import { AppDispatch, RootState } from "../store/store";
-import { useGetMeQuery } from "../redux/apiServices";
+import { RootState } from "../store/store";
 
 const Navbar = () => {
-  const dispatch = useDispatch<AppDispatch>();
   const user = useSelector((state: RootState) => state.user);
-  const { data: userData } = useGetMeQuery({});
-  const navigate = useNavigate();
 
   useEffect(() => {
-    if (userData && userData.success) {
-      console.log("from navbar useEffect", userData);
-      dispatch(addUserState(userData.data));
-    } else {
-      navigate("/sign-in");
+    if (user) {
+      console.log("from navbar useEffect", user);
     }
-  }, [userData, dispatch, navigate]);
+  }, [user]);
   return (
     <>
       <div className="flex justify-center md:h-20">
@@ -32,14 +22,18 @@ const Navbar = () => {
               <label className="text-2xl">Super Pizza</label>
             </Link>
           </div>
-          {user ? (
+          {user.email ? (
             <div>
               <p>{user.email}</p>
             </div>
           ) : (
             <div className="flex-row hidden gap-4 md:flex">
-              <Link to={"/sign-in"}>Sign In</Link>
-              <Link to={"/sign-up"}>Sign Up</Link>
+              <p>
+                <Link to={"/sign-in"}>Sign In</Link>
+              </p>
+              <p>
+                <Link to={"/sign-up"}>Sign Up</Link>
+              </p>
             </div>
           )}
         </div>
