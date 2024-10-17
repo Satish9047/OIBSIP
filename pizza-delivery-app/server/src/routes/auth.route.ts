1496daa9d909b617ed20661e629b0dc5f88c0890
Satish9047 <satishprajapati930@gmail.com>
2024-10-17 21:09:16 +0545

    127: Use Verify Refresh Token Middleware

--- pizza-delivery-app/server/src/routes/auth.route.ts
+++ pizza-delivery-app/server/src/routes/auth.route.ts
@@ -8,7 +8,7 @@ import {
   logoutHandler,
   getMeHandler,
 } from "../controllers/auth.controller";
-import { verifyToken } from "../middlewares/jwt.middleware";
+import { verifyRefreshToken, verifyToken } from "../middlewares/jwt.middleware";
 import { signInSchema, signUpSchema } from "../schema/user.schema";
 import { validateData } from "../middlewares/validation.middleware";
 
@@ -18,7 +18,7 @@ authRouter.post("/sign-up", validateData(signUpSchema), signUpHandler);
 authRouter.post("/sign-in", validateData(signInSchema), signInHandler);
 authRouter.get("/me", verifyToken, getMeHandler);
 authRouter.post("/verify-user", verifyToken, verificationHandler);
-authRouter.get("/refresh-token", refreshTokenHandler);
+authRouter.get("/refresh-token", verifyRefreshToken, refreshTokenHandler);
 authRouter.get("/logout", verifyToken, logoutHandler);
 
 export { authRouter };
