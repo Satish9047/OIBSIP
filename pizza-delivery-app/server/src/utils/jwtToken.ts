f45f4c35838d57fda2c37f1f6bafd218bad6c389
Satish9047 <satishprajapati930@gmail.com>
2024-10-17 21:10:31 +0545

    128: Use refreshJwtSecret and accessJwtSecret to create Jwt Tokens

--- pizza-delivery-app/server/src/utils/jwtToken.ts
+++ pizza-delivery-app/server/src/utils/jwtToken.ts
@@ -2,17 +2,21 @@ import jwt from "jsonwebtoken";
 import { appConfig } from "../configs/app.config";
 
 export const createJwtToken = (id: string, email: string, role: string) => {
-  const accessToken = jwt.sign({ id, email, role }, appConfig.jwtSecret, {
+  const accessToken = jwt.sign({ id, email, role }, appConfig.accessJwtSecret, {
     expiresIn: "10min",
   });
-  const refreshToken = jwt.sign({ id, email, role }, appConfig.jwtSecret, {
-    expiresIn: "1d",
-  });
+  const refreshToken = jwt.sign(
+    { id, email, role },
+    appConfig.refreshJwtSecret,
+    {
+      expiresIn: "1d",
+    }
+  );
   return { accessToken, refreshToken };
 };
 
 export const createAccessToken = (id: string, email: string, role: string) => {
-  const accessToken = jwt.sign({ id, email, role }, appConfig.jwtSecret, {
+  const accessToken = jwt.sign({ id, email, role }, appConfig.accessJwtSecret, {
     expiresIn: "10min",
   });
   return accessToken;
