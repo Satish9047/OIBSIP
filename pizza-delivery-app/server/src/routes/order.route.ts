dba3ed473095be83bd736fe2e07879be5244884e
Satish9047 <satishprajapati930@gmail.com>
2024-10-18 11:01:34 +0545

    136: Add get order by user Route

--- pizza-delivery-app/server/src/routes/order.route.ts
+++ pizza-delivery-app/server/src/routes/order.route.ts
@@ -5,12 +5,14 @@ import {
   createOrderHandler,
   updateOrderDeliverHandler,
   deleteOrderHandler,
+  getOrderByUserHandler,
 } from "../controllers/order.controller";
 import { verifyToken, verifyAdmin } from "../middlewares/jwt.middleware";
 
 const orderRouter = Router();
 
 orderRouter.get("/", verifyAdmin, getAllOrderHandler);
+orderRouter.get("/user", verifyToken, getOrderByUserHandler);
 orderRouter.post("/", verifyToken, createOrderHandler);
 orderRouter.put("/:id", verifyAdmin, updateOrderDeliverHandler);
 orderRouter.delete("/:id", verifyAdmin, deleteOrderHandler);
