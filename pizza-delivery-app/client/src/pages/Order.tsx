877e8aee99b48dbf07264f8b9e6a0d346f8bef87
Satish9047 <satishprajapati930@gmail.com>
2024-10-20 15:52:58 +0545

    148: Use SelectBtn

--- pizza-delivery-app/client/src/pages/Order.tsx
+++ pizza-delivery-app/client/src/pages/Order.tsx
@@ -1,65 +1,51 @@
+import SelectBtn from "../components/SelectBtn";
 import { Button } from "../components/ui/button";
+import { IPizzaRecipe, IRecipe } from "../interface/app.interface";
+import { useGetAllRecipeQuery } from "../redux/api/apiServices";
 
 function Order() {
+  const { data } = useGetAllRecipeQuery({});
+  if (!data || !data.data) {
+    console.log("No data available");
+    return (
+      <div>
+        <p>No data</p>
+      </div>
+    );
+  }
+  const { pizzaBase, sauce, cheese, veggies, nonVeg }: IPizzaRecipe = data.data;
+
   return (
     <div className="flex flex-col items-center  gap-5 md:w-11/12 min-h-[calc(100vh-5rem)] mx-auto p-4">
       <div className="w-full">
         <div>
-          <h2>Make your Custom Pizza order</h2>
+          <h2 className="text-3xl font-bold">Make your Custom Pizza order</h2>
         </div>
         <div className="flex flex-col gap-6 md:flex-row">
           <aside className="md:w-8/12">
-            <form action="">
-              <h3>Customize Your pizza</h3>
+            <form action="" className="flex flex-col gap-4">
               <div>
                 <label htmlFor="">Choose your desire Pizza Base</label>
-                <div>
-                  <label>NewYork Style </label>
-                  <label>California Style</label>
-                  <label>Italian Style</label>
-                  <label>Plain</label>
-                  <label>Greek Style</label>
-                </div>
+                <SelectBtn recipes={pizzaBase as unknown as IRecipe[]} />
               </div>
               <div>
                 <label htmlFor="">Choose your desire sauce</label>
-                <div>
-                  <label htmlFor="">A Sauce</label>
-                  <label htmlFor="">B Sauce</label>
-                  <label htmlFor="">C Sauce</label>
-                  <label htmlFor="">D Sauce</label>
-                  <label htmlFor="">E Sauce</label>
-                </div>
+                <SelectBtn recipes={sauce as unknown as IRecipe[]} />
               </div>
               <div>
                 <label htmlFor="">Choose your desire cheese type</label>
-                <div>
-                  <label htmlFor="">Goat Cheese</label>
-                  <label htmlFor="">A Cheese</label>
-                  <label htmlFor="">B Cheese</label>
-                  <label htmlFor="">c Cheese</label>
-                  <label htmlFor="">D Cheese</label>
-                </div>
+                <SelectBtn recipes={cheese as unknown as IRecipe[]} />
               </div>
               <div>
                 <label htmlFor="">Choose your desire Veggies</label>
-                <div>
-                  <label htmlFor="">Mushroom</label>
-                  <label htmlFor="">Paneer</label>
-                  <label htmlFor="">Cauliflower</label>
-                  <label htmlFor="">Chilli Peeper</label>
-                  <label htmlFor="">Green Chilli</label>
-                </div>
+                <SelectBtn recipes={veggies as unknown as IRecipe[]} />
               </div>
               <div>
                 <label htmlFor="">Choose your desire NonVeg</label>
-                <div>
-                  <label htmlFor="">Bacon</label>
-                  <label htmlFor="">Sausage</label>
-                </div>
+                <SelectBtn recipes={nonVeg as unknown as IRecipe[]} />
               </div>
               <div className="text-right">
-                <Button className="bg-orange-500 rounded-md  hover:bg-orange-600">
+                <Button className="bg-orange-500 rounded-md hover:bg-orange-600">
                   Make Order
                 </Button>
               </div>
