d34742acb279b2a9b8ec6b23d4f916d10b493198
Satish9047 <satishprajapati930@gmail.com>
2024-10-20 15:52:09 +0545

    147: Add interface for IGetUserOrder + IOrder

---
+++ pizza-delivery-app/client/src/interface/order.Interface.ts
@@ -0,0 +1,55 @@
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+}
+
+export interface IPizzaBase {
+  _id: string;
+  name: string;
+}
+
+export interface ISauceType {
+  _id: string;
+  name: string;
+}
+
+export interface ICheeseType {
+  _id: string;
+  name: string;
+}
+
+export interface IVeggie {
+  _id: string;
+  name: string;
+}
+
+export interface INonVeg {
+  _id: string;
+  name: string;
+}
+
+export interface IOrder {
+  _id: string;
+  user: User;
+  pizzaBase: IPizzaBase;
+  sauceType: ISauceType;
+  cheeseType: ICheeseType;
+  veggies: IVeggie[];
+  nonVeg: INonVeg[];
+  quantity: number;
+  isDelivered: boolean;
+  paid: boolean;
+  price: number;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+export interface IGetUserOrder {
+  data: IOrder[];
+  message: string;
+  status: number;
+  success: boolean;
+}
