3ed69c1bec4c73bb57b776d429f6d8d35d499629
Satish9047 <satishprajapati930@gmail.com>
2024-10-20 15:51:14 +0545

    146: Add Interface for IRecipeResponse

--- pizza-delivery-app/client/src/interface/app.interface.ts
+++ pizza-delivery-app/client/src/interface/app.interface.ts
@@ -31,6 +31,38 @@ export interface IAuthResponse {
   success: boolean;
 }
 
+export interface IRecipe {
+  _id: string;
+  name: string;
+  totalQuantity: number;
+  price: number;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
+export interface IPizzaRecipe {
+  pizzaBase: IPizzaRecipeType[];
+  sauce: IPizzaRecipeType[];
+  cheese: IPizzaRecipeType[];
+  veggies: IPizzaRecipeType[];
+  nonVeg: IPizzaRecipeType[];
+}
+
+export interface IPizzaRecipeType {
+  data: IRecipe;
+  message: string;
+  status: number;
+  success: boolean;
+}
+
+export interface IRecipeResponse {
+  data: IPizzaRecipe;
+  message: string;
+  status: number;
+  success: boolean;
+}
+
 export interface IResponse {
   data: unknown;
   message: string;
