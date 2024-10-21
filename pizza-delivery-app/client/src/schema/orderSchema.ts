d362a5e251e255db252871d34b9c5d3a8f98c153
Satish9047 <satishprajapati930@gmail.com>
2024-10-21 21:24:30 +0545

    168: Update Order Schema with array  to string

--- pizza-delivery-app/client/src/schema/orderSchema.ts
+++ pizza-delivery-app/client/src/schema/orderSchema.ts
@@ -4,7 +4,7 @@ export const orderSchema = z.object({
   pizzaBaseId: z.string(),
   pizzaSauceId: z.string(),
   pizzaCheeseId: z.string(),
-  pizzaVeggiesIds: z.array(z.string()),
-  pizzaNonVegIds: z.array(z.string()),
-  quantity: z.number().min(1, "Quantity must be at least 1"),
+  pizzaVeggiesIds: z.string(),
+  pizzaNonVegIds: z.string(),
+  quantity: z.string().min(1, "Quantity must be at least 1"),
 });
