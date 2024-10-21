507f1831e5b8def795ebf79c7a8a41343a405cf0
Satish9047 <satishprajapati930@gmail.com>
2024-10-21 21:29:00 +0545

    173: Update loga

--- pizza-delivery-app/server/src/middlewares/jwt.middleware.ts
+++ pizza-delivery-app/server/src/middlewares/jwt.middleware.ts
@@ -15,7 +15,7 @@ const verifyToken = async (
   next: NextFunction
 ) => {
   const token = req.cookies?.accessToken;
-  // console.log("from token verify", req.cookies);
+  console.log("from token verify", req.cookies);
 
   if (!token) {
     return next(new ApiError(401, "Unauthorized"));
