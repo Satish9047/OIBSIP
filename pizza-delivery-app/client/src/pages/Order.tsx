98f9cabf828abad50393338026b7d7065f11d6ac
Satish9047 <satishprajapati930@gmail.com>
2024-10-21 21:16:40 +0545

    164: Update order page with orderPizza state + add zod + formHook

--- pizza-delivery-app/client/src/pages/Order.tsx
+++ pizza-delivery-app/client/src/pages/Order.tsx
@@ -1,19 +1,36 @@
+import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import SelectBtn from "../components/SelectBtn";
 import { Button } from "../components/ui/button";
 import { IPizzaRecipe, IRecipe } from "../interface/app.interface";
 import { useGetAllRecipeQuery } from "../redux/api/apiServices";
+import { orderSchema } from "../schema/orderSchema";
+import { useForm } from "react-hook-form";
+
+type PizzaOrderFormData = z.infer<typeof orderSchema>;
 
 function Order() {
   const { data } = useGetAllRecipeQuery({});
+  const [orderPizza, setOrderPizza] = useState<PizzaOrderFormData | null>(null);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PizzaOrderFormData>({ resolver: zodResolver(orderSchema) });
+
   if (!data || !data.data) {
-    console.log("No data available");
-    return (
-      <div>
-        <p>No data</p>
-      </div>
-    );
+    return null;
   }
   const { pizzaBase, sauce, cheese, veggies, nonVeg }: IPizzaRecipe = data.data;
+
+  const onSubmit = (formData: PizzaOrderFormData) => {
+    setOrderPizza(formData);
+    console.log("order pizza ", orderPizza);
+    console.log("pizza order submitted:", formData);
+  };
 
   return (
     <div className="flex flex-col items-center  gap-5 md:w-11/12 min-h-[calc(100vh-5rem)] mx-auto p-4">
@@ -23,26 +40,57 @@ function Order() {
         </div>
         <div className="flex flex-col gap-6 md:flex-row">
           <aside className="md:w-8/12">
-            <form action="" className="flex flex-col gap-4">
+            <form
+              action=""
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col gap-4"
+            >
               <div>
                 <label htmlFor="">Choose your desire Pizza Base</label>
-                <SelectBtn recipes={pizzaBase as unknown as IRecipe[]} />
+                <SelectBtn
+                  recipes={pizzaBase as unknown as IRecipe[]}
+                  register={register("pizzaBaseId")}
+                  errors={errors.pizzaBaseId?.message}
+                />
               </div>
               <div>
                 <label htmlFor="">Choose your desire sauce</label>
-                <SelectBtn recipes={sauce as unknown as IRecipe[]} />
+                <SelectBtn
+                  recipes={sauce as unknown as IRecipe[]}
+                  register={register("pizzaSauceId")}
+                  errors={errors.pizzaSauceId?.message}
+                />
               </div>
               <div>
                 <label htmlFor="">Choose your desire cheese type</label>
-                <SelectBtn recipes={cheese as unknown as IRecipe[]} />
+                <SelectBtn
+                  recipes={cheese as unknown as IRecipe[]}
+                  register={register("pizzaCheeseId")}
+                  errors={errors.pizzaCheeseId?.message}
+                />
               </div>
               <div>
                 <label htmlFor="">Choose your desire Veggies</label>
-                <SelectBtn recipes={veggies as unknown as IRecipe[]} />
+                <SelectBtn
+                  recipes={veggies as unknown as IRecipe[]}
+                  register={register("pizzaVeggiesIds")}
+                  errors={errors.pizzaVeggiesIds?.message}
+                />
               </div>
               <div>
                 <label htmlFor="">Choose your desire NonVeg</label>
-                <SelectBtn recipes={nonVeg as unknown as IRecipe[]} />
+                <SelectBtn
+                  recipes={nonVeg as unknown as IRecipe[]}
+                  register={register("pizzaNonVegIds")}
+                  errors={errors.pizzaNonVegIds?.message}
+                />
+              </div>
+              <div>
+                <label htmlFor="">Quantity</label>
+                <input type="phone" {...register("quantity")} />
+                {errors.quantity && (
+                  <p className="text-red-500">{errors.quantity.message}</p>
+                )}
               </div>
               <div className="text-right">
                 <Button className="bg-orange-500 rounded-md hover:bg-orange-600">
@@ -61,5 +109,4 @@ function Order() {
     </div>
   );
 }
-
 export default Order;
