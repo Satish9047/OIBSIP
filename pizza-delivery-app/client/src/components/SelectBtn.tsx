0b848a693cfbf20154c5bac37d3984e762539c9a
Satish9047 <satishprajapati930@gmail.com>
2024-10-21 21:12:35 +0545

    162; Update SelectBtn with zod + react hook form

--- pizza-delivery-app/client/src/components/SelectBtn.tsx
+++ pizza-delivery-app/client/src/components/SelectBtn.tsx
@@ -1,20 +1,33 @@
+import { UseFormRegisterReturn } from "react-hook-form";
 import { IRecipe } from "../interface/app.interface";
 
 interface SelectBtnProps {
   recipes: IRecipe[];
+  register: UseFormRegisterReturn;
+  errors?: string;
 }
-const SelectBtn = ({ recipes }: SelectBtnProps) => {
+const SelectBtn = ({ recipes, register, errors }: SelectBtnProps) => {
   return (
-    <select className="flex flex-wrap gap-6">
+    <div className="flex flex-wrap gap-6">
       {recipes?.map((item) => (
-        <option
-          key={item._id}
-          className="w-1/6 h-20 p-6 text-lg text-center bg-orange-400 "
-        >
-          {item.name}
-        </option>
+        <div className="py-6" key={item._id}>
+          <input
+            id={item._id}
+            {...register}
+            type="radio"
+            value={item._id}
+            className="hidden peer"
+          />
+          <label
+            htmlFor={item._id}
+            className="w-1/6 h-20 p-6 text-lg text-center bg-orange-200 peer-checked:bg-orange-500"
+          >
+            {item.name}
+          </label>
+        </div>
       ))}
-    </select>
+      {errors && <p className="text-red-500">{errors}</p>}
+    </div>
   );
 };
 export default SelectBtn;
