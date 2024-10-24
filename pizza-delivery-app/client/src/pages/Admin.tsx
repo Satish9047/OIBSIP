1fa4b96abc10cf9af8577f3d36175f332edb60c5
Satish9047 <satishprajapati930@gmail.com>
2024-10-24 19:02:54 +0545

    182: Add admin Page

--- pizza-delivery-app/client/src/pages/Admin.tsx
+++ pizza-delivery-app/client/src/pages/Admin.tsx
@@ -1,5 +1,28 @@
 const Admin = () => {
-  return <div>This is admin page</div>;
+  return (
+    <div className="flex flex-col items-center  gap-5 md:w-11/12 min-h-[calc(100vh-5rem)] mx-auto p-4">
+      <div className="flex flex-row w-full gap-6">
+        <aside className="md:w-3/12">
+          <div>
+            <div>
+              <h2>Admin Panel</h2>
+            </div>
+            <div>
+              <div>Orders</div>
+              <div>Dashboard</div>
+              <div>Inventory</div>
+              <div>Users</div>
+            </div>
+          </div>
+        </aside>
+        <aside className="md:w-9/12">
+          <div>
+            <div>This is Dashboard</div>
+          </div>
+        </aside>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
