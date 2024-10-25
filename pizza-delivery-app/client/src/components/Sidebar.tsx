48ac7e3fa065a64225c5b823ec385d2099dc2dc4
Satish9047 <satishprajapati930@gmail.com>
2024-10-25 21:06:27 +0545

    192: Update SideBar with Admin Links too

--- pizza-delivery-app/client/src/components/Sidebar.tsx
+++ pizza-delivery-app/client/src/components/Sidebar.tsx
@@ -1,5 +1,35 @@
-import { Link, Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, Outlet } from "react-router-dom";
+
+import { removeUserState } from "../redux/state/userSlice";
+import { useEffect } from "react";
+import { useSignOutMutation } from "../redux/api/apiServices";
+import { RootState } from "../store/store";
+
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.user);
+
+  const [signOut] = useSignOutMutation();
+
+  useEffect(() => {
+    if (!user.isVerified) {
+      navigate("/sign-in");
+    }
+  }, [navigate, user]);
+
+  const handleSignOut = async () => {
+    try {
+      const res = await signOut({}).unwrap();
+      console.log("from sidebar", res);
+      dispatch(removeUserState());
+      localStorage.removeItem("persist:root");
+      navigate("/sign-in");
+    } catch (error) {
+      console.error("Sign out failed:", error);
+    }
+  };
   return (
     <main className="flex flex-col md:flex-row w-11/12 min-h-[calc(100vh-5rem)] mx-auto p-4">
       <div className="flex flex-row w-full min-h-screen gap-6">
@@ -9,16 +39,31 @@ const Sidebar = () => {
               <h3>Panel</h3>
             </div>
             <div>
-              <Link to={"/dashboard"}>Dashboard</Link>
+              <Link to={"/user/dashboard"}>Dashboard</Link>
             </div>
             <div>
               <Link to={"/user/profile"}>Profile</Link>
             </div>
             <div>
-              <Link to={"/user/order"}>Custom Order</Link>
+              <Link to={"/user/your-orders"}>Your Orders</Link>
             </div>
             <div>
-              <Link to={"/sign-out"}>Sign Out</Link>
+              <Link to={"/user/order"}>Custom Order</Link>
+            </div>
+            {user.role === "admin" && (
+              <div className="flex flex-col gap-6">
+                <div>
+                  <Link to={"/admin/view-orders"}>Orders</Link>
+                </div>
+                <div>
+                  <Link to={"/admin/inventory"}>Inventory</Link>
+                </div>
+              </div>
+            )}
+            <div>
+              <Link to={"/sign-in"} onClick={handleSignOut}>
+                Sign Out
+              </Link>
             </div>
           </div>
         </aside>
