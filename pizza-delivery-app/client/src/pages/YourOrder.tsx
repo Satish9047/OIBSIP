64ff5b87bca02f31001c30e4d16d3846506e528e
Satish9047 <satishprajapati930@gmail.com>
2024-10-25 21:12:25 +0545

    199: Add UserOrder Page

---
+++ pizza-delivery-app/client/src/pages/YourOrder.tsx
@@ -0,0 +1,42 @@
+import {
+  Table,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../components/ui/table";
+import OrderList from "../components/OrderList";
+import { useGetUserOrderQuery } from "../redux/api/apiServices";
+import { IOrder } from "../interface/order.Interface";
+
+const YourOrder = () => {
+  const { data: userOrders } = useGetUserOrderQuery({});
+  const userOrdersList: IOrder[] = userOrders?.data || [];
+  return (
+    <main className="w-full p-2">
+      <h3 className="font-semibold">Your Order</h3>
+      <div>
+        {userOrdersList.length < 1 ? (
+          <div className="flex flex-col items-center justify-center h-full">
+            <p>Your Order is Empty</p>
+          </div>
+        ) : (
+          <Table className="w-full text-lg">
+            <TableHeader>
+              <TableRow>
+                <TableHead>S.N.</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Delivered</TableHead>
+                <TableHead>Quantity</TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead>Paid</TableHead>
+              </TableRow>
+            </TableHeader>
+            <OrderList userOrder={userOrdersList} />
+          </Table>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default YourOrder;
