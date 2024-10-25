5f73d02a8542d9f940bd14d0386678ddf5556611
Satish9047 <satishprajapati930@gmail.com>
2024-10-25 21:09:22 +0545

    196: Remove User's Order From Profile

--- pizza-delivery-app/client/src/pages/Profile.tsx
+++ pizza-delivery-app/client/src/pages/Profile.tsx
@@ -1,38 +1,17 @@
-import {
-  useGetUserOrderQuery,
-  useGetUserQuery,
-} from "../redux/api/apiServices";
-import OrderList from "../components/OrderList";
-import { IOrder } from "../interface/order.Interface";
+import { useGetUserQuery } from "../redux/api/apiServices";
 
 const Profile = () => {
   const { data } = useGetUserQuery({});
-  const { data: userOrders } = useGetUserOrderQuery({});
-
-  console.log("User data:", data?.data);
-  console.log("User orders:", userOrders);
-
-  if (!data || !data.data) {
-    console.log("no data");
-    return null;
-  }
-  if (!userOrders || !userOrders.data) {
-    console.log("no order data");
-    return null;
-  }
-
-  const userOrdersList: IOrder[] = userOrders?.data || [];
-  console.log(userOrdersList);
 
   console.log("from profile", data?.data);
   return (
-    <div className="flex flex-col items-center md:w-11/12 gap-5 min-h-[calc(100vh-5rem)] mx-auto ">
+    <div className="flex flex-col items-center md:w-full gap-5 min-h-[calc(100vh-5rem)] ">
       <div className="w-full p-4">
         <h2>My Profile</h2>
       </div>
-      <div className="flex flex-col items-center justify-between w-11/12 p-4 ">
-        <div className="flex flex-col justify-between gap-12 md:flex-row">
-          <aside>
+      <div className="flex flex-col items-center w-full p-4 ">
+        <div className="flex flex-col gap-12 md:flex-row">
+          <aside className="flex flex-col gap-12 md:flex-row">
             <figure>
               <img
                 src="/images/pizza1.png"
@@ -40,7 +19,7 @@ const Profile = () => {
                 className="w-96 "
               />
             </figure>
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col justify-center gap-4">
               <div className="flex flex-row gap-2">
                 <img
                   src="/icon/profile.svg"
@@ -68,22 +47,6 @@ const Profile = () => {
                 <p>{data?.data.address}</p>
               </div>
             </div>
-          </aside>
-          <aside className="w-full p-2">
-            <h3 className="font-semibold">Your Order</h3>
-            <table className="w-full text-lg border-2">
-              <thead>
-                <tr>
-                  <th>S.N.</th>
-                  <th>Name</th>
-                  <th>Delivered</th>
-                  <th>Quantity</th>
-                  <th>Date</th>
-                  <th>Paid</th>
-                </tr>
-              </thead>
-              <OrderList userOrder={userOrdersList} />
-            </table>
           </aside>
         </div>
       </div>
