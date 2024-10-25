9b814d2a68d67bffbc01f1ee81e30cca1d4c7edf
Satish9047 <satishprajapati930@gmail.com>
2024-10-25 21:08:08 +0545

    194: Add Forget Password Page

---
+++ pizza-delivery-app/client/src/pages/ForgetPassword.tsx
@@ -0,0 +1,85 @@
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast, ToastContainer } from "react-toastify";
+
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import { resetPasswordSchema } from "../schema/resetPasswordSchema";
+import { useForgetPasswordMutation } from "../redux/api/apiServices";
+
+type ForgetPassword = z.infer<typeof resetPasswordSchema>;
+const ForgetPassword = () => {
+  const navigate = useNavigate();
+  const [forgetPasswordRequest, { isLoading }] = useForgetPasswordMutation();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<ForgetPassword>({
+    resolver: zodResolver(resetPasswordSchema),
+  });
+
+  const onSubmit = async (formData: ForgetPassword) => {
+    console.log(formData);
+    try {
+      const { data } = await forgetPasswordRequest(formData);
+
+      if (data?.success === true) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      toast.error("An error occurred. Please try again later.");
+      console.error(error);
+    } finally {
+      reset();
+    }
+  };
+  return (
+    <div className=" w-11/12 min-h-[calc(100vh-5rem)] mx-auto p-4 flex items-center justify-center">
+      <div className="flex items-center justify-center w-full max-w-3xl p-8 rounded-lg shadow-md">
+        <form
+          className="flex flex-col gap-4 md:w-8/12"
+          onSubmit={handleSubmit(onSubmit)}
+          max-w-3xl
+        >
+          <ToastContainer />
+          <div>
+            <h2>Forget Password</h2>
+          </div>
+          <div className="space-y-4">
+            <div>
+              <p>
+                Enter your email address to send link for creating new password
+              </p>
+            </div>
+            <div className="my-4">
+              <label htmlFor="email">Email</label>
+              <Input
+                type="email"
+                {...register("email")}
+                className="border-stone-300 focus:outline-none focus:border-orange-500"
+              />
+              {errors?.email && (
+                <p className="text-red-500">{`${errors.email.message}`}</p>
+              )}
+            </div>
+          </div>
+          <div>
+            <Button
+              type="submit"
+              className="bg-orange-500 rounded-md hover:bg-orange-600"
+            >
+              {isLoading ? "Loading ..." : "Reset Password"}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ForgetPassword;
