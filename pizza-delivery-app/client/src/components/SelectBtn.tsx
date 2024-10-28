328048dbc43d76ee1870ceba8ce4fedecb0069f3
Satish9047 <satishprajapati930@gmail.com>
2024-10-28 22:14:46 +0545

    231: Update SelectBtn with default value comes from dashboard page

--- pizza-delivery-app/client/src/components/SelectBtn.tsx
+++ pizza-delivery-app/client/src/components/SelectBtn.tsx
@@ -5,8 +5,14 @@ interface SelectBtnProps {
   recipes: IRecipe[];
   register: UseFormRegisterReturn;
   errors?: string;
+  defaultValue?: string;
 }
-const SelectBtn = ({ recipes, register, errors }: SelectBtnProps) => {
+const SelectBtn = ({
+  recipes,
+  register,
+  errors,
+  defaultValue,
+}: SelectBtnProps) => {
   return (
     <div className="flex flex-col flex-wrap gap-3">
       <div className="flex flex-wrap gap-6">
@@ -18,10 +24,14 @@ const SelectBtn = ({ recipes, register, errors }: SelectBtnProps) => {
               type="radio"
               value={item._id}
               className="hidden peer"
+              defaultChecked={defaultValue === item._id}
+              disabled={defaultValue ? defaultValue !== item._id : undefined}
             />
             <label
               htmlFor={item._id}
-              className="w-1/6 h-20 p-6 text-lg text-center bg-orange-200 peer-checked:bg-orange-500"
+              className={`w-1/6 h-20 p-6 rounded-lg shadow-xl text-lg text-center bg-orange-200 ${
+                defaultValue === item._id ? "bg-orange-300" : ""
+              } peer-checked:bg-orange-300`}
             >
               {item.name}
             </label>
