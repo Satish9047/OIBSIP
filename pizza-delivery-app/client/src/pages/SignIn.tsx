3f7e0817d9589adf3f60fbe5df6527873a3aff75
Satish9047 <satishprajapati930@gmail.com>
2024-10-28 22:31:37 +0545

    241 :  Update with proper error handling

--- pizza-delivery-app/client/src/pages/SignIn.tsx
+++ pizza-delivery-app/client/src/pages/SignIn.tsx
@@ -9,52 +9,41 @@ import { Button } from "../components/ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { signInSchema } from "../schema/signInSchema";
 import { useSignInMutation } from "../redux/api/apiServices";
-import { ISignIn } from "../interface/app.interface";
+import { IAuthResponse, ISignIn } from "../interface/app.interface";
 import { addUserState } from "../redux/state/userSlice";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../store/store";
 
 const SignIn = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const [signIn, { isLoading }] = useSignInMutation();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
   } = useForm<ISignIn>({ resolver: zodResolver(signInSchema) });
-  const [signIn, { isLoading }] = useSignInMutation();
-  const navigate = useNavigate();
-  const dispatch = useDispatch<AppDispatch>();
 
   const onSubmit = async (formData: ISignIn) => {
-    console.log(formData);
     try {
       const signInResponse = await signIn(formData).unwrap();
-      console.log("Sign Up successful:", signInResponse);
+
       if (signInResponse.success && signInResponse.data) {
         dispatch(addUserState(signInResponse.data));
         navigate("/user/dashboard");
       }
-      if ("status" in signInResponse && signInResponse.status === 403) {
-        navigate("/validate");
-      }
+
       toast.success(signInResponse.message);
     } catch (error: unknown) {
       console.error("Sign In failed:", error);
-      if (
-        typeof error === "object" &&
-        error !== null &&
-        "status" in error &&
-        error.status === 403
-      ) {
-        console.log("going to verifyUser");
+
+      if ((error as IAuthResponse).status === 403) {
         navigate("/validate");
       }
 
-      if (typeof error === "object" && error !== null && "message" in error) {
-        toast.error((error as { message: string }).message);
-      } else {
-        toast.error("An unknown error occurred");
-      }
+      toast.error((error as IAuthResponse).message);
       reset();
     }
   };
@@ -62,7 +51,9 @@ const SignIn = () => {
     <main className="flex items-center justify-center min-h-screen">
       <section className="w-full max-w-3xl p-8 rounded-lg shadow-md">
         <div className="mb-6 text-center">
-          <h1 className="text-3xl font-bold">Sign In Into Super Pizza</h1>
+          <h1 className="text-3xl font-bold text-orange-500">
+            Sign In Into Super Pizza
+          </h1>
           <ToastContainer />
         </div>
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
@@ -87,7 +78,7 @@ const SignIn = () => {
                   className="border-stone-300 focus:outline-none focus:border-orange-500"
                 />
                 {errors?.email && (
-                  <p className="text-red-500">{`${errors.email.message}`}</p>
+                  <p className="text-red-300">{`${errors.email.message}`}</p>
                 )}
               </div>
               <div className="my-4">
@@ -100,7 +91,7 @@ const SignIn = () => {
                   className="border-stone-300 focus:outline-none focus:border-orange-500"
                 />
                 {errors?.password && (
-                  <p className="text-red-500">{`${errors.password.message}`}</p>
+                  <p className="text-red-300">{`${errors.password.message}`}</p>
                 )}
               </div>
               <div className="flex justify-between mt-6">
@@ -108,21 +99,24 @@ const SignIn = () => {
                   Go to{" "}
                   <Link
                     to={"/sign-up"}
-                    className="text-blue-600 hover:font-bold"
+                    className="text-sm text-blue-500 hover:text-blue-600"
                   >
                     Sign Up
                   </Link>
                 </p>
                 <Button
                   type="submit"
-                  className="bg-orange-500 rounded-md hover:bg-orange-600 "
+                  className="bg-orange-300 rounded-md hover:bg-orange-400 "
                 >
                   {isLoading ? "Loading..." : "Sign In"}
                 </Button>
               </div>
               <div>
                 <p>
-                  <Link to={"/forgot-password"}>
+                  <Link
+                    to={"/forgot-password"}
+                    className="text-sm text-blue-500 hover:text-blue-600"
+                  >
                     Forgot Password? Click Here
                   </Link>
                 </p>
