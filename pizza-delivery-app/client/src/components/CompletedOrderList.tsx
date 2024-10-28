1ece3a964d5bee77d6232e2eb525561ccb65a01d
Satish9047 <satishprajapati930@gmail.com>
2024-10-28 22:11:39 +0545

    228: Add CompletedOr de rLis t Component

---
+++ pizza-delivery-app/client/src/components/CompletedOrderList.tsx
@@ -0,0 +1,28 @@
+import { IAllOrder } from "../interface/order.Interface";
+
+const CompletedOrderList = ({
+  completedData,
+}: {
+  completedData: IAllOrder[];
+}) => {
+  return (
+    <div>
+      {completedData.map((item, index) => (
+        <div key={item._id}>
+          <p>{item.user.name}</p>
+          <label htmlFor="">{index}</label>
+          <p>
+            {item.pizzaBase.name}, {item.sauceType.name}, {item.cheeseType.name}
+            , {item.veggies[0].name}, {item.nonVeg[0].name}
+          </p>
+          <p>Total Price: {item.price}</p>
+          <p>{item.quantity}</p>
+          <p> {item.paid}</p>
+          <p>{item.isDelivered}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CompletedOrderList;
