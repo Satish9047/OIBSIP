90ce194909ca2ddca27762f9748069cbb1213594
Satish9047 <satishprajapati930@gmail.com>
2024-10-28 22:10:31 +0545

    227: Update Route + add Admin route

--- pizza-delivery-app/client/src/App.tsx
+++ pizza-delivery-app/client/src/App.tsx
@@ -7,7 +7,6 @@ import {
 import Home from "./pages/Home";
 import Order from "./pages/Order";
 import Pizza from "./pages/Pizza";
-import Admin from "./pages/Admin";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
@@ -18,25 +17,32 @@ import Sidebar from "./components/Sidebar";
 import ResetPassword from "./pages/ResetPassword";
 import ForgetPassword from "./pages/ForgetPassword";
 import YourOrder from "./pages/YourOrder";
+import AllOrders from "./pages/AllOrders";
+import Inventory from "./pages/Inventory";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<Navbar />}>
+        {/* Public Routes */}
         <Route path="/" element={<Home />}></Route>
         <Route path="/forgot-password" element={<ForgetPassword />}></Route>
         <Route
           path="/reset-password/:userId/:token"
           element={<ResetPassword />}
         ></Route>
+
+        {/* User Routes */}
         <Route path="user" element={<Sidebar />}>
           <Route path="dashboard" element={<Dashboard />}></Route>
           <Route path="profile" element={<Profile />}></Route>
-
           <Route path="order" element={<Order />}></Route>
           <Route path="your-orders" element={<YourOrder />}></Route>
           <Route path="pizza" element={<Pizza />}></Route>
-          <Route path="admin" element={<Admin />}></Route>
+
+          {/* Admin Routes */}
+          <Route path="all-orders" element={<AllOrders />}></Route>
+          <Route path="inventory" element={<Inventory />}></Route>
         </Route>
       </Route>
       <Route path="sign-in" element={<SignIn />}></Route>
